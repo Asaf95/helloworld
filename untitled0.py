8e1a8e4cd284470414f06fdc5c0bdb1b90104036
Asaf Ben-Menachem <71070915+Asaf95@users.noreply.github.com>
2021-03-05 16:36:34 +0200

    Create untitled0.py

---
+++ untitled0.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  5 16:32:40 2021
+
+@author: Asaf
+"""
+
+x = input("Type a number: ")
+y = input("Type another number: ")
+
+sum = int(x) + int(y)
+
+print("The sum is: ", sum)
